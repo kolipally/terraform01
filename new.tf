cbce7408f48925fa437dabde997bb1a2e92d8458
kolipally <93519952+kolipally@users.noreply.github.com>
2021-11-11 15:17:29 +0530

    Create new.tf

---
+++ new.tf
@@ -0,0 +1,24 @@
+provider "google" {
+project = var.project
+region = "us-central1"
+}
+variable "machine" {
+type = string
+default = "f1-micro"
+}
+variable "project" {
+type = string
+}
+resource "google_compute_instance" "myvm" {
+name = "myfirstvm"
+machine_type = var.machine
+zone = "us-central1-a"
+network_interface {
+network = "default"
+}
+boot_disk {
+initialize_params {
+image = "debian-cloud/debian-10"
+}
+}
+}
